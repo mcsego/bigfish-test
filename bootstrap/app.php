9e401f4ac8f18e4719f8ba94cbb99aa8f8d9419a
miskolczicsego <csego.miskolczi@innonic.com>
2021-04-12 22:42:29 +0200

    install graphql vendor + set up configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,9 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+
+$app->register(Rebing\GraphQL\GraphQLLumenServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
